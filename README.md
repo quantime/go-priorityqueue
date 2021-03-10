c919889a22f8d839e234c542c92c42dcc0c86671
Burke Livingston <burkel24@gmail.com>
2021-03-10 14:42:33 -0500

    Add Queuable interface with Clean method to queue item type (#3)

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 A priority queue implementation on top of container/heap
 
 Forked from https://github.com/jupp0r/go-priority-queue
+
--- lib/pq/priorityqueue_test.go
+++ lib/pq/priorityqueue_test.go
@@ -7,21 +7,44 @@ import (
 	lib "github.com/quantime/go-priorityqueue/lib/pq"
 )
 
+type TestItem struct {
+	val  string
+	prio float64
+}
+
+var counter int
+
+func (t TestItem) Clean() {
+	counter++
+}
+
 func TestPriorityQueue(t *testing.T) {
 	pq := lib.New()
-	elements := []float64{5, 3, 7, 8, 6, 2, 9}
-	for _, e := range elements {
-		pq.Insert(e, e)
+	elements := []TestItem{
+		TestItem{"A", 5},
+		TestItem{"B", 3},
+		TestItem{"C", 7},
+		TestItem{"D", 8},
+		TestItem{"E", 6},
+		TestItem{"F", 2},
+		TestItem{"G", 9},
 	}
 
-	sort.Float64s(elements)
+	for _, e := range elements {
+		pq.Insert(e, e.prio)
+	}
+
+	sort.SliceStable(elements, func(i, j int) bool {
+		return elements[i].prio < elements[j].prio
+	})
+
 	for _, e := range elements {
 		item, err := pq.Pop()
 		if err != nil {
 			t.Fatalf(err.Error())
 		}
 
-		i := item.(float64)
+		i := item.(TestItem)
 		if e != i {
 			t.Fatalf("expected %v, got %v", e, i)
 		}
@@ -29,46 +52,55 @@ func TestPriorityQueue(t *testing.T) {
 }
 
 func TestPriorityQueueUpdate(t *testing.T) {
+	a := TestItem{"A", 3}
+	b := TestItem{"B", 4}
+
 	pq := lib.New()
-	pq.Insert("foo", 3)
-	pq.Insert("bar", 4)
-	pq.UpdatePriority("bar", 2)
+	pq.Insert(a, a.prio)
+	pq.Insert(b, b.prio)
+	pq.UpdatePriority(b, 2)
 
 	item, err := pq.Pop()
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 
-	if item.(string) != "bar" {
+	if item.(TestItem) != b {
 		t.Fatal("priority update failed")
 	}
 }
 
 func TestPriorityQueueLen(t *testing.T) {
+	a := TestItem{"A", 3}
+	b := TestItem{"B", 4}
+
 	pq := lib.New()
 	if pq.Len() != 0 {
 		t.Fatal("empty queue should have length of 0")
 	}
 
-	pq.Insert("foo", 1)
-	pq.Insert("bar", 1)
+	pq.Insert(a, a.prio)
+	pq.Insert(b, b.prio)
 	if pq.Len() != 2 {
 		t.Fatal("queue should have lenght of 2 after 2 inserts")
 	}
 }
 
 func TestDoubleAddition(t *testing.T) {
+	a := TestItem{"A", 3}
+	b := TestItem{"B", 4}
+
 	pq := lib.New()
-	pq.Insert("foo", 2)
-	pq.Insert("bar", 3)
-	pq.Insert("bar", 1)
+	pq.Insert(a, 2)
+	pq.Insert(b, 3)
+	pq.Insert(b, 1)
 
 	if pq.Len() != 2 {
 		t.Fatal("queue should ignore inserting the same element twice")
 	}
 
 	item, _ := pq.Pop()
-	if item.(string) != "foo" {
+	if item.(TestItem) != a {
 		t.Fatal("queue should ignore duplicate insert, not update existing item")
 	}
 }
@@ -82,29 +114,51 @@ func TestPopEmptyQueue(t *testing.T) {
 }
 
 func TestUpdateNonExistingItem(t *testing.T) {
+	a := TestItem{"A", 3}
+	b := TestItem{"B", 4}
+
 	pq := lib.New()
 
-	pq.Insert("foo", 4)
-	pq.UpdatePriority("bar", 5)
+	pq.Insert(a, 4)
+	pq.UpdatePriority(b, 5)
 
 	if pq.Len() != 1 {
 		t.Fatal("update should not add items")
 	}
 
 	item, _ := pq.Pop()
-	if item.(string) != "foo" {
-		t.Fatalf("update should not overwrite item, expected \"foo\", got \"%v\"", item.(string))
+	if item.(TestItem) != a {
+		t.Fatalf("update should not overwrite item, expected \"foo\", got \"%v\"", item.(TestItem))
 	}
 }
 
 func TestMaxSize(t *testing.T) {
+	a := TestItem{"A", 3}
+	b := TestItem{"B", 4}
+	c := TestItem{"C", 5}
+	d := TestItem{"D", 6}
+
 	pq := lib.NewWithMaxSize(3)
-	pq.Insert("foo", 1)
-	pq.Insert("bar", 2)
-	pq.Insert("baz", 3)
-	pq.Insert("bat", 1)
+	pq.Insert(a, 1)
+	pq.Insert(b, 2)
+	pq.Insert(c, 3)
+	pq.Insert(d, 4)
 
 	if pq.Len() != 3 {
 		t.Fatal("max size violated")
+	}
+}
+
+func TestMaxSizeClean(t *testing.T) {
+	counter = 0
+	a := TestItem{"A", 3}
+	b := TestItem{"B", 4}
+
+	pq := lib.NewWithMaxSize(1)
+	pq.Insert(a, 1)
+	pq.Insert(b, 2)
+
+	if counter == 0 {
+		t.Fatalf("clean should be called when overflow item is popped")
 	}
 }
